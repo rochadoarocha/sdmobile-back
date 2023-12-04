89dcebbd6b873c77cbfe0ddbe22879427ac2ddd0
Matheus Felipe <matheusbarros9107@gmail.com>
2023-12-04 19:46:08 -0300

    Create PostDeleteDTO.java

---
+++ src/main/java/com/sdmobile/sdmobileback/dto/PostDeleteDTO.java
@@ -0,0 +1,13 @@
+package com.sdmobile.sdmobileback.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostDeleteDTO {
+	
+	private Integer postId;
+	private String username;
+
+}
