e3a52289cd346f7af94579b17966624de75600c5
Matheus Felipe <matheusbarros9107@gmail.com>
2023-12-04 17:44:20 -0300

    Create IUserRepository.java

---
+++ src/main/java/com/sdmobile/sdmobileback/repositories/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.sdmobile.sdmobileback.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sdmobile.sdmobileback.entities.User;
+
+public interface IUserRepository extends JpaRepository <User, Integer> {
+
+}
+	
