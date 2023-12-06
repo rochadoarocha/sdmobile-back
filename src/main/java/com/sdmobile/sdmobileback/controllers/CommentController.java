d91dba6bedc0e99285ea6167d856b3702909bdd5
Matheus Felipe <matheusbarros9107@gmail.com>
2023-12-05 22:23:34 -0300

    hot fix create comment

--- src/main/java/com/sdmobile/sdmobileback/controllers/CommentController.java
+++ src/main/java/com/sdmobile/sdmobileback/controllers/CommentController.java
@@ -1,4 +1,4 @@
-	package com.sdmobile.sdmobileback.controllers;
+package com.sdmobile.sdmobileback.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
